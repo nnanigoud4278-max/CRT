44116ac76a08ee04231380577ca72a0b817b2069
nnanigoud4278-max <nnanigoud4278@gmail.com>
2026-03-13 09:21:43 +0530

    Delete CRT directory

--- CRT/4th sem/python/assignments/MO2_Logic_Building_and_Patterns/AST04/task.py
+++
@@ -1,9 +0,0 @@
-def right_triangle(n: int) -> str:
-    result = ""
-    for i in range(1, n + 1):
-        result += "*" * i + "\n"
-    return result.rstrip("\n")  
-
-if __name__ == "__main__":
-    n = int(input())
-    print(right_triangle(n))
